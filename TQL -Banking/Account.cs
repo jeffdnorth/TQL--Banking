f42d8a9e050dfe9d52b6d9f273435ed09a0ce524
Jeff North <jnorth@tql.com>
2021-05-20 16:04:23 -0400

    Add project files.

---
+++ TQL -Banking/Account.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TQL__Banking
+{
+    class Account
+    {
+        public string AccountNumber { get; private set; }
+        public decimal Balance { get; private set; }
+        public string RoutingNumber { get; set; }
+        public string Description { get; set; }
+        /*   description is just a name for various accounts ??      */
+
+        public bool Deposit(decimal amount)
+        {
+            Balance = Balance + amount;
+            return true;
+        }
+
+        public bool Withdraw(decimal amount)
+        {
+            if (amount < 0)
+            {
+                Console.WriteLine($"Amount must be GTE zero");
+                return false;
+            }
+
+
+            if (amount > Balance)
+            {
+                Console.WriteLine($"Amount must be GT zero");
+                return false;
+            }
+            Balance = Balance - amount;
+            return true;
+
+        }
+
+
+        public bool Transfer(decimal amount, Account toAccount)
+        { 
+            var success = this.Withdraw(amount);
+            if(success == true)
+              {  toAccount.Deposit(amount);
+                return true;
+              }
+
+            return false;
+
+        }
+
+
+
+
+    }
+}
---
+++ TQL -Banking/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TQL__Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var acct1 = new Account();
+            acct1.Deposit (500);
+            acct1.Withdraw(200);
+            acct1.Withdraw(600);
+            acct1.Deposit(-400);
+            Console.WriteLine($"Balance is {acct1.Balance}");
+
+            var acct2 = new Account();
+            acct1.Transfer(1000, acct2);
+            Console.WriteLine($"Balance is {acct1.Balance}");
+            Console.WriteLine($"Balance is {acct2.Balance}");
+        }
+    }
+}
